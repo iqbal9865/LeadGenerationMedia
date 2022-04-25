d94e18ef36d8b8f2e0d8d9d747393616c1791f2b
iqbal9865 <iqbalahmed9865@gmail.com>
2022-04-25 13:36:25 +0600

    working on lead generation media home

--- Home/index.js
+++ Home/index.js
@@ -29,4 +29,28 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var swiper = new Swiper(".project-section", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+  
   
